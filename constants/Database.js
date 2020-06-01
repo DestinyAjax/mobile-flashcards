ebc1d2fd781fd80e252bd06e3bc005d103cbf216
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-06-01 16:39:20 +0100

    Added database endpoints with AsyncStorage

---
+++ constants/Database.js
@@ -0,0 +1,70 @@
+import { AsyncStorage } from "react-native";
+
+const storeDeck = async (payload) => {
+    const deck = JSON.stringify(payload);
+    await AsyncStorage.setItem("flashcards", deck);
+}
+
+export const getDecks = () => {
+    return new Promise(async (resolve, reject) => {
+        const decks = await AsyncStorage.getItem("flashcards");
+        setTimeout(() => resolve({...JSON.parse(decks)}), 1000);
+    });
+}
+
+export const getDeck = id => {
+    return new Promise(async (resolve, reject) => {
+        const all_decks = await getDecks();
+        if (all_decks) {
+            const deck = all_decks[id];
+            resolve(deck);
+        }
+        else {
+            resolve({});
+        }
+    })
+}
+
+export const saveDeckTitle = async (title) => {
+    if (title) {
+        const all_decks = await getDecks();
+        const new_deck = {
+            [title]: {
+                title: `${title}`,
+                questions: []
+            }
+        };
+
+        if (all_decks) {
+            const newDecks = {
+                ...all_decks,
+                new_deck
+            };
+            await storeDeck(newDecks);
+        }
+        else {
+            const newDecks = Object.assign({}, new_deck);
+            await storeDeck(newDecks);
+        }
+    }
+}
+
+export const addCardToDeck = (title, card) => {
+    return new Promise(async (resolve, reject) => {
+        const all_decks = await getDecks();
+        if (all_decks) {
+            const current_deck = all_decks[title];
+            let prev_questions = current_deck.questions;
+            const updated_desks = {
+                ...all_decks,
+                [title]: {
+                    title: `${title}`,
+                    questions: prev_questions.concat(card)
+                }
+            }
+
+            await storeDeck(updated_desks);
+            resolve();
+        }
+    });
+}
