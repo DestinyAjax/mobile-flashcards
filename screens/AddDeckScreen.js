dd64eb491d917735a2b0aec28812dcd17020c8b9
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-06-01 18:43:33 +0100

    Added save deck page

--- screens/AddDeckScreen.js
+++ screens/AddDeckScreen.js
@@ -1,11 +1,44 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TextInput, Button, Text } from "react-native";
+import { saveDeckTitle } from "../constants/Database";
 
 class AddDeckScreen extends React.Component {
+
+    state = {
+        title: ''
+    };
+
+    onChangeText = (event) => {
+        const title = event.target.value;
+        this.setState(() => ({
+            title
+        }));
+    };
+
+    onHandleSubmit = async () => {
+        const { title } = this.state;
+        await saveDeckTitle(title);
+    }
+
+
     render() {
+        const { title } = this.state;
+
         return (
             <View style={styles.rootContainer}>
-                
+                <View style={styles.textWrapper}>
+                    <Text>What is the title of your new deck?</Text>
+                </View>
+                <View style={styles.inputWrapper}>
+                    <TextInput 
+                        onChange={this.onChangeText} 
+                        value={title} 
+                        style={styles.input} 
+                    />
+                </View>
+                <View style={styles.btnWrapper}>
+                    <Button title="Submit" onPress={} style={styles.btn} />
+                </View>
             </View>
         );
     }
@@ -14,7 +47,21 @@ class AddDeckScreen extends React.Component {
 const styles = StyleSheet.create({
     rootContainer: {
         flex: 1
-    }
+    },
+    textWrapper: {
+        marginBottom: 10
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    inputWrapper: {
+
+    },
+    input: {},
+    btnWrapper: {},
+    btn: {}
 });
 
 export default AddDeckScreen;
